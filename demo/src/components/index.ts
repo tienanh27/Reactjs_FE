8f1b2be6e852ccd788bdc307e102d4cf705899dd
ThaiBao011002 <thaibao011002@gmail.com>
2023-07-19 18:27:44 +0700

    delete demo

--- demo/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import CustomInput from "./custom-input";
-import Button from "./button";
-
-export { Button, CustomInput };
--- demo/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import Login from "./login";
-import Register from "./register";
-import Home from "./home";
-
-export { Login, Register, Home };
