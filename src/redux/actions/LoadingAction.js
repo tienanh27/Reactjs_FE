3eebe1e8281f8e6ebd196f5ee9dd10fd9f10396a
ThaiBao011002 <thaibao011002@gmail.com>
2023-07-19 15:39:08 +0700

    fix

--- src/redux/actions/LoadingAction.js
+++
@@ -1,9 +0,0 @@
-import { HIDE_LOADING, SHOW_LOADING } from "../types/LoadingType";
-
-export const showLoadingAction = () => ({
-	type: SHOW_LOADING,
-});
-
-export const hideLoadingAction = () => ({
-	type: HIDE_LOADING,
-});
